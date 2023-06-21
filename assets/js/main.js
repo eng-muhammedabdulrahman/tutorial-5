8decee2aa8d728bdba8321c005c1a78fd4fdec2c
eng-muhammedabdulrahman <eng-muhammed.abdulrahman@outlook.com>
2023-06-21 21:37:25 +0300

    updata

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 const menu_bar = document.querySelector(".menu-bar");
-const list = document.querySelector(".list");
+const menu_List = document.querySelector(".list");
+
+menu_bar.addEventListener("click", function () {
+
+  menu_List.classList.toggle("toggle_menu_bar");
+  
+});
